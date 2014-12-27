666fd4f92f49547f4ee53d1f810c7a6af03735d9
mattlykins <matt@mattlykins.com>
2014-12-27 06:54:55 -0500

    set up db methods

--- AtomicDataHive/classes/sql.py
+++ AtomicDataHive/classes/sql.py
@@ -1,23 +1,55 @@
 import sqlite3
 
-con = sqlite3.connect('stout.db')
+def dbConnect(fileName):
+    con = sqlite3.connect(fileName)
+    return con
 
-def createDB():
+def dbCreate(con):
     
     c = con.cursor()
+    
+    c.execute('''DROP TABLE IF EXISTS levels''')
     
     c.execute('''CREATE TABLE IF NOT EXISTS levels
         (id INTEGER PRIMARY KEY,
         energy REAL NOT NULL,
-        g REAL NOT NULL)''')
+        g REAL NOT NULL,
+        speciesid INTEGER NOT NULL,
+        FOREIGN KEY(speciesid) REFERENCES species(id))''')
+    
+    c.execute('''DROP TABLE IF EXISTS transitions''')
     
     c. execute('''CREATE TABLE IF NOT EXISTS transitions
-        (id TEXT PRIMARY KEY,
+        (id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
         lo INTEGER NOT NULL,
         hi INTEGER NOT NULL,
         FOREIGN KEY(lo) REFERENCES levels(id),
         FOREIGN KEY(hi) REFERENCES levels(id))''')
+    
+    c.execute('''DROP TABLE IF EXISTS species''')
+    c.execute('''CREATE TABLE IF NOT EXISTS species
+        (id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        z INTEGER NOT NULL,
+        ion INTEGER NOT NULL)''')
     return c
 
-def dbCommit():
+
+def dbCommit(con):
     con.commit()
+    
+def dbAddSpecies(c,name,z,ion):
+    c.execute("INSERT INTO species(name,z,ion) VALUES (?,?,?)",(name,z,ion,))
+    SET_DEBUG_DB = True
+    if(SET_DEBUG_DB):
+        print("Inserted %s, Z = %i, ION = %i into db" % (name,z,ion,))
+        
+def dbAddLevel(c,index,energy,g,speciesid):
+    c.execute("INSERT INTO levels(id,energy,g,speciesid) VALUES (?,?,?,?)",(index,energy,g,speciesid,))
+    SET_DEBUG_DB = True
+    if(SET_DEBUG_DB):
+        c.execute("SELECT name FROM species WHERE species.id=?", (speciesid,))
+        tSpec = c.fetchone()
+        print("Inserted %i\t%e\t%.1f: from %s" % (index,energy,g, tSpec[0]))
+        
--- AtomicDataHive/main.py
+++ AtomicDataHive/main.py
@@ -11,12 +11,16 @@ import os,re
 
 O2 = species(8,2)
 Fe2 = species(26,2)
+
+
 #stoutData = importStout()
 #readStout(Fe2,"")
 readStout(O2,"")
 
+con = dbConnect('stout.db')
+c = dbCreate(con)
 
-
+dbAddSpecies(c,'O_2',8,2)
 #print(Fe2.transitions['1:2'].collision.collData)
 #print(O2.transitions['1:5'].collision.collData)
 #print(O2.levels[1].energy)
@@ -25,21 +29,23 @@ readStout(O2,"")
 
 #stout.print(stoutData)
 
-c = createDB()
 
-# for level in O2.levels:    
-#     c.execute("INSERT INTO levels(id,energy,g) VALUES (?,?,?)",(level.index,level.energy,level.g))
-#     
-# dbCommit()
-# 
-# for key,value in O2.transitions.items():
-#     c.execute("INSERT INTO transitions(id,lo,hi) VALUES (?,?,?)",(key,value.lo.index,value.hi.index))
-#      
-# dbCommit()
+
+for level in O2.levels:    
+    #c.execute("INSERT INTO levels(id,energy,g,speciesid) VALUES (?,?,?,?)",(level.index,level.energy,level.g,0))
+    dbAddLevel(c,int(level.index),float(level.energy),float(level.g),int(1))
+ 
+for key,value in O2.transitions.items():
+    c.execute("INSERT INTO transitions(name,lo,hi) VALUES (?,?,?)",(key,value.lo.index,value.hi.index))
+      
+dbCommit(con)
 lower = 2
 higher = 3
 id = str(lower) + ":" + str(higher),
-c.execute('SELECT * FROM transitions WHERE id=?',id)
+c.execute('SELECT * FROM transitions')
 
-print(c.fetchall())
+x = c.fetchall()
+
+for i in x:
+    print(i)
 
